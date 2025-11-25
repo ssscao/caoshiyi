44140c58103a955fef1c2f2f03b83ebfdb486f6a
ssscao <127209883+ssscao@users.noreply.github.com>
2025-11-26 00:44:44 +0800

    Update NetEaseMusic.tsx

--- app/(main)/NetEaseMusic.tsx
+++ app/(main)/NetEaseMusic.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export function NetEaseMusic() {
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"，
+  };
+
   return (
-    <div 
-      style={{ 
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center',
-      }}
-    >
-      <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=110 src="//music.163.com/outchain/player?type=0&id=6673286917&auto=1&height=90"></iframe>
+    <div style={containerStyle}>
+      <iframe
+        frameBorder="0"
+        marginWidth="0"
+        marginHeight="0"
+        width="330"
+        height="110"
+        src="//music.163.com/outchain/player?type=0&id=6673286917&auto=1&height=90"
+        allow="autoplay"
+      />
     </div>
   );
 }
