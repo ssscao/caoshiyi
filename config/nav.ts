3360358643ed1de8a720244ba0c40d183c9343be
ssscao <127209883+ssscao@users.noreply.github.com>
2026-05-19 16:25:03 +0800

    Update nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -1,7 +1,7 @@
 export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '博客' },
-  { href: '/projects', text: '项目' },
+  { href: '/projects', text: 'Usefulinks' },
   { href: '/guestbook', text: '留言墙' },
   { href: '/ama', text: '杂谈' },
   //{ href: '/about'， text: '关于' },
