8434e2df5f61f4c38c0bf0622993b9480e9ae05d
ssscao <127209883+ssscao@users.noreply.github.com>
2026-05-19 17:35:14 +0800

    Update Footer.tsx

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -63,7 +63,7 @@ async function TotalPageViews() {
   )
 }
 
-输入 VisitorGeolocation = {
+type VisitorGeolocation = {
   country: string
   city?: string
   flag: string
@@ -115,10 +115,12 @@ export async function Footer() {
               <Newsletter subCount={`${subs?.subCount ?? '0'}`} />
             </div>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-               <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
-                
+              <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
+                &copy; {new Date().getFullYear()} {/* 源码来源：
+                <PeekabooLink href="https://github.com/ssscao ">
+                  GitHub
+                </PeekabooLink> */}
               </p>
-              
               <Links />
             </div>
           </Container.Inner>
